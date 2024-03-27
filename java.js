efae0a76b0bd60f4bf5c864461cb9773e26e10bc
memmedlileman <memmedileman02@gmail.com>
2024-03-27 18:26:11 +0400

    task13

---
+++ java.js
@@ -0,0 +1,89 @@
+//task1
+let eded = +prompt("ededi daxil edin");
+let say = 0;
+for (let i = 2; i <= eded; i++) {
+    if(eded% i ===0){
+       say++;
+    }
+}
+if (say === 2) {
+    console.log(i + " sade ededdir.");
+}
+
+//task2
+// let eded = +prompt(" eded daxil edin:");
+//   let cem = 0;
+ 
+//   for (let i = 1; i < eded; i++) {
+//       if (eded % i === 0) {
+//           cem += i;
+//       }
+//  }
+//  if (cem === eded) {
+//      console.log(eded + " mükəmməldir.");
+//  } 
+//  else {
+//      console.log(eded + " mükəmməl deyil.");
+//   }
+
+
+//task3
+// let eded = +prompt(" eded daxil edin:");
+// let eded2 = +prompt(" eded daxil edin:");
+
+// for (let i = eded; i < eded2; i++) {
+//       let cem = 0;
+//     for (let bölən = 1; bölən < i; bölən++) {
+//         if (i % bölən === 0) {
+//             cem += bölən;
+//         }
+//     }
+//     if (cem === i) {
+//         console.log(i + " mükəmməl ədəddir.");
+//     }
+//  }
+
+
+
+//task4
+// let eded1 = +prompt(" 1ci eded daxil edin:");
+// let eded2 = +prompt(" 2ci eded daxil edin:");
+// for (let i = eded1; i <= eded2; i++) {
+//     let say = 0;
+//     for (let j = 1; j <= i; j++) {
+//         if (i % j === 0) {
+//             say++;
+//         }
+//     }
+//     if (say === 2) {
+//         console.log(i + " sade ededdir.");
+//     }
+// }
+
+
+
+
+
+//task5
+// let eded1 = +prompt(" eded daxil edin:");
+// let eded2 = +prompt(" eded daxil edin:");
+// for( let i =eded1  ; i < eded2 ; i ++){
+//       if(i % 10===7){
+//              console.log(i);
+//    }
+// }
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
